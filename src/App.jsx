700b757663d43bf3b4716ce93c6ae455180abb90
netwp1rate <netwp1rate@pop-os.localdomain>
2023-08-08 12:46:06 -0300

    deleting cabins

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
+import GlobalStyles from "./styles/GlobalStyles"
 import Dashboard from "./pages/Dashboard"
 import Cabins from "./pages/Cabins"
 import Users from "./pages/Users"
@@ -9,8 +12,15 @@ import Account from "./pages/Account"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./ui/AppLayout"
+import { Toaster } from "react-hot-toast"
 
-import GlobalStyles from "./styles/GlobalStyles"
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000
+		}
+	}
+})
 
 const router = createBrowserRouter([
 	{
@@ -31,9 +41,29 @@ const router = createBrowserRouter([
 
 export default function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<RouterProvider router={router} />
-		</>
+
+			<Toaster
+				position="top-center"
+				gutter={12}
+				containerStyle={{ margin: "8px" }}
+				toastOptionns={{
+					success: {
+						duration: 3000
+					},
+					error: {
+						duration: 5000
+					},
+					style: {
+						fontSize: "16px",
+						maxWidth: "500px",
+						padding: "16px 24px",
+						backgroundColor: "var(--color-gre-"
+				}}
+			/>
+		</QueryClientProvider>
 	)
 }
