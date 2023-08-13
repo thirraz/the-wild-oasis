81656d50eff1131b90bb8e625b986ec81da364db
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-08-13 12:33:24 -0300

    detecting a click outside the modal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import { useForm } from "react-hook-form"
 import { useCreateCabin } from "./useCreateCabin"
 import { useEditCabin } from "./useEditCabin"
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
 	const { createCabin, isCreating } = useCreateCabin()
 	const { editCabin, isEditing } = useEditCabin()
 
@@ -28,14 +28,20 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 			editCabin(
 				{ newCabinData: { ...data, image }, id: editId },
 				{
-					onSuccess: () => reset()
+					onSuccess: (data) => {
+						reset()
+						onCloseModal?.()
+					}
 				}
 			)
 		else
 			createCabin(
 				{ ...data, image: image },
 				{
-					onSuccess: () => reset()
+					onSuccess: (data) => {
+						reset()
+						onCloseModal?.()
+					}
 				}
 			)
 	}
@@ -47,7 +53,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 	}
 
 	return (
-		<Form onSubmit={handleSubmit(onSubmit, onError)}>
+		<Form
+			onSubmit={handleSubmit(onSubmit, onError)}
+			type={onCloseModal ? "modal" : "regular"}
+		>
 			<FormRow label="Cabin name" error={errors?.name?.message}>
 				<Input
 					type="text"
@@ -133,7 +142,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
 			<FormRow>
 				{/* type is an HTML attribute! */}
-				<Button variation="secondary" type="reset">
+				<Button
+					variation="secondary"
+					type="reset"
+					onClick={() => onCloseModal?.()}
+				>
 					Cancel
 				</Button>
 				<Button disabled={isCreating}>
